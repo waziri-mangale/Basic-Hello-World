c7b4994db35a8bf59f4c2502df2db4c08be2f68f
waziri-mangale <wazirimangale@gmail.com>
2023-07-31 20:15:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+</head>
+<body>
+    <H1>
+        Hello, Worlddddd!!!!!!.....
+</H1>
+<?php
+$var = "Mangale Waziri Hamisi";
+echo 'Hello, ' . $var;
+?>
+</body>
+</html>
